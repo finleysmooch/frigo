960b22ff75238c9747ea440051558d44467f8090
finleysmooch <tommorley33@gmail.com>
2025-09-30 13:05:39 -0700

    Update supabase.ts
    update url

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = 'https://siaawxcgyghupmgufkn.supabase.co';
+const supabaseUrl = 'https://siaawxcgyghuphwgufkn.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpYWF3eGNneWdodXBod2d1ZmtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNDU2MTYsImV4cCI6MjA3NDgyMTYxNn0.xqcDMCqKm5Vch-tVByvSbHgWjNqGFQ8gwQeEC8-Tvyc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
